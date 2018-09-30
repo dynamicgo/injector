cd74961b2061841e8cd1ce11fe33da3409a6603b
yayanyang <yayanyang@gmail.com>
2018-09-30 10:35:16 +0800

    update

--- injector_test.go
+++ injector_test.go
@@ -1,12 +1,14 @@
 package injector
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 type testA struct {
+	V int
 }
 
 type testB struct {
@@ -15,16 +17,32 @@ type testB struct {
 }
 
 func TestInjectorGet(t *testing.T) {
-	Register("a1", &testA{})
-	Register("a2", &testA{})
+
+	a11 := &testA{1}
+	a22 := &testA{2}
+
+	require.NotEqual(t, a11, a22)
+
+	Register("a1", a11)
+	Register("a2", a22)
 
 	var a1 *testA
 
 	require.True(t, Get("a1", &a1))
 
+	require.NotNil(t, a1)
+
+	require.Equal(t, a11, a1)
+
 	var a2 *testA
 
 	require.True(t, Get("a2", &a2))
+
+	require.NotNil(t, a2)
+
+	require.Equal(t, a22, a2)
+
+	require.NotEqual(t, a1, a2)
 
 	require.False(t, Get("test", &a1))
 
@@ -39,5 +57,8 @@ func TestInjectorGet(t *testing.T) {
 	require.NoError(t, Inject(b))
 
 	require.Equal(t, b.A, a1)
-	require.Equal(t, b.A, a2)
+	require.Equal(t, b.A1, a2)
+
+	println(fmt.Sprintf("%p", b.A))
+	println(fmt.Sprintf("%p", b.A1))
 }
